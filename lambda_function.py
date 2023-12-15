7ee1423b4c7b66523fd506429d3ef2e346d01688
蘿蔔 <55518831+iambjlu@users.noreply.github.com>
2023-12-15 21:17:58 +0800

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Date: 2023/11/14
-Author: @1chooo(Hugo ChunHo Lin)
-E-mail: hugo970217@gmail.com
-Version: v0.1.0
-"""
 
 import os
 import json
@@ -38,23 +31,22 @@ def lambda_handler(event, context):
                 event.reply_token,
                 reply_messages
             )
-        elif event_text == "我想知道 AWS 創辦故事":
+        elif event_text == "吃什麼":
             reply_messages = [
                 TextSendMessage(
-                    text=f'AWS ☁️ 的創辦故事始於 2003 年，當時亞馬遜公司的 Jeff Bezos 意識到他們在建設強大的內部基礎設施方面取得了不錯的進展！'
-                ),
-                ImageSendMessage(
-                    original_content_url = "https://line-workshop-test.s3.amazonaws.com/jeff_bezos.jpeg",
-                    preview_image_url = "https://line-workshop-test.s3.amazonaws.com/jeff_bezos.jpeg",
+                    text=f'火鍋'
                 ),
                 TextSendMessage(
-                    text=f'他認識到這個基礎設施可以成為一個強大的雲端運算平台，能夠為其他企業提供服務。於是在 2006 年，AWS ☁️ 正式推出，開始提供雲端服務。'
+                    text=f'拉麵'
                 ),
                 TextSendMessage(
-                    text=f'AWS ☁️ 的成功與其開放性、靈活性和不斷創新的企業文化有關。'
+                    text=f'壽司'
                 ),
                 TextSendMessage(
-                    text=f'這一切都源於 Jeff Bezos 對技術和未來的敏銳洞察力✨，他能看到潛在的機會並推動公司朝著新的方向發展。'
+                    text=f'咖哩飯'
+                ),
+                TextSendMessage(
+                    text=f'Pizza'
                 ),
             ]
                 
@@ -62,23 +54,22 @@ def lambda_handler(event, context):
                 event.reply_token,
                 reply_messages
             )
-        elif event_text == "我想知道 AWS 提供的業務":
+        elif event_text == "人物介紹":
             reply_messages = [
                 TextSendMessage(
-                    text=f'AWS ☁️ 提供多樣化的雲端運算服務，包括計算（如 EC2 與 Lambda）、儲存（如 S3）、資料庫（如 RDS）、機器學習、人工智慧、物聯網、區塊鏈等。'
+                    text=f'我叫做羅。'
                 ),
                 TextSendMessage(
-                    text=f'舉例來說，EC2 提供虛擬伺服器，Lambda 則提供無需管理伺服器的程式碼執行環境。同時，S3 提供無限儲存空間，RDS 提供受管理的資料庫服務。API Gateway 可建立、部署和管理安全的 API，讓您輕鬆構建和監控應用程式介面。'
-                ),
-                ImageSendMessage(
-                    original_content_url = "https://line-workshop-test.s3.amazonaws.com/aws_services.png",
-                    preview_image_url = "https://line-workshop-test.s3.amazonaws.com/aws_services.png",
+                    text=f'我叫做沈'
                 ),
                 TextSendMessage(
-                    text=f'AWS ☁️ 致力於為客戶提供完整、可擴展且高度安全的雲端解決方案。'
+                    text=f'我叫做Neil。'
                 ),
                 TextSendMessage(
-                    text=f'且 AWS ☁️ 的多樣性和高度可靠性確保您可以找到符合您業務需求的解決方案。'
+                    text=f'我叫做FKT。'
+                ),
+                TextSendMessage(
+                    text=f'我叫柏均'
                 ),
             ]
                 
@@ -86,23 +77,22 @@ def lambda_handler(event, context):
                 event.reply_token,
                 reply_messages
             )
-        elif event_text == "我想知道 AWS 的16項領導力準則":
+        elif event_text == "配對地點":
             reply_messages = [
                 TextSendMessage(
-                    text=f'AWS ☁️ 的 16 項領導力準則是文化核心價值觀的體現，著重於滿足客戶需求並超越期望，以及追求卓越而非滿足現狀。'
-                ),
-                ImageSendMessage(
-                    original_content_url = "https://line-workshop-test.s3.amazonaws.com/aws_leadership.jpg",
-                    preview_image_url = "https://line-workshop-test.s3.amazonaws.com/aws_leadership.jpg",
+                    text=f'信義商圈。'
                 ),
                 TextSendMessage(
-                    text=f'同時，這些準則強調了團隊合作、創新、效率和對變革的開放態度，成為公司文化中的重要支柱。'
+                    text=f'中山商圈'
                 ),
                 TextSendMessage(
-                    text=f'透過這些準則，AWS ☁️ 激勵著員工發揮最佳表現，持續挑戰自我，並不斷追求個人與團隊的成長。'
+                    text=f'板橋耶誕城。'
                 ),
                 TextSendMessage(
-                    text=f'這些準則不僅是指導方針，更是推動公司進步和持續發展的動力所在。'
+                    text=f'南紡購物中心'
+                ),
+                TextSendMessage(
+                    text=f'華泰名品城'
                 ),
             ]
                 
@@ -110,23 +100,22 @@ def lambda_handler(event, context):
                 event.reply_token,
                 reply_messages
             )
-        elif event_text == "我想知道 AWS 未來的發展方向":
+        elif event_text == "任務挑戰":
             reply_messages = [
                 TextSendMessage(
-                    text=f'AWS ☁️ 將持續引領雲端運算發展，擴展服務和解決方案以滿足客戶需求。'
+                    text=f'雙人吃pocky。'
                 ),
                 TextSendMessage(
-                    text=f'未來將深入研究人工智慧、機器學習等，提供更智能、靈活的解決方案。'
+                    text=f'頭套絲襪'
                 ),
                 TextSendMessage(
-                    text=f'AWS ☁️ 致力於提升雲端基礎設施性能和安全性，保障客戶資料安全。'
+                    text=f'對視十秒。'
                 ),
                 TextSendMessage(
-                    text=f'AWS ☁️ 積極推動環保和可持續發展，尋求綠色能源方案與客戶攜手實現可持續目標'
+                    text=f'十連拍標'
                 ),
-                ImageSendMessage(
-                    original_content_url = "https://line-workshop-test.s3.amazonaws.com/aws_future.jpg",
-                    preview_image_url = "https://line-workshop-test.s3.amazonaws.com/aws_future.jpg",
+               TextSendMessage(
+                    text=f'交互蹲跳20下'
                 ),
             ]
                 
@@ -134,43 +123,43 @@ def lambda_handler(event, context):
                 event.reply_token,
                 reply_messages
             )
-        elif event_text == "我想了解 AWS LINE BOT 開發團隊":
+        elif event_text == "App介紹":
             reply_messages = [
                 TextSendMessage(
-                    text=f'嗨！👋 \n我們是第五屆 AWS Educate 校園大使'
+                    text=f'當冬季的寒風吹拂，凜冬將至，萬物似乎都沉浸在一片靜謐之中。但在這寒冷的季節裡，有一個溫暖的節日正悄悄接近——一年一度的聖誕夜。'
                 ),
                 TextSendMessage(
-                    text=f'我是你的 AWS 小幫手，將帶你探索 Amazon Web Services 的無限可能。'
+                    text=f'街道上開始裝飾著五彩繽紛的燈飾和聖誕樹，每個角落都洋溢著節日的歡樂和期待。這是一個充滿奇蹟和歡笑的時刻，也是一個關於愛與被愛的夢想。'
                 ),
                 TextSendMessage(
-                    text=f'將陪你一同踏上雲端運算的冒險之旅。AWS 提供豐富多元的雲端服務，從運算到儲存，應有盡有。'
+                    text=f'然而，在這充滿歡樂的季節裡，也許你還是一個人。孤單地走在燈火闌珊的街頭，看著身邊的情侶手牽手，心中不免泛起一絲寂寞。但別擔心，聖誕節不僅僅是一個家庭團聚的時刻，也是一個尋找愛情的好機會。在這個節日裡，許多人都渴望找到屬於自己的另一半，去分享這份節日的喜悅。'
                 ),
                 TextSendMessage(
-                    text=f'我會與你分享 AWS 的最新動態、技術見解，以及 AWS Educate 獨家的學習體驗，讓我們一同迎接科技的挑戰吧！🌟💡'
+                    text=f'因此，我們特別為你準備了一個特別的機會——一次認識優質男士的機會。這些男士不僅有著良好的品德和積極的生活態度，還擁有各自獨特的魅力和才華。他們都在尋找一位能夠共度美好時光的伴侶。'
                 ),
-                ImageSendMessage(
-                    original_content_url = "https://1225-line-workshop-demo-g0.s3.amazonaws.com/aws_educate.png",
-                    preview_image_url = "https://1225-line-workshop-demo-g0.s3.amazonaws.com/aws_educate.png",
+                TextSendMessage(
+                    text=f'此刻，為自己勇敢一次，給自己一個重獲新生的機會。不要讓這個聖誕夜成為另一個孤單的夜晚。來加入我們，認識這些優質男士，也許你會在這個聖誕節找到那個特別的他，與他一起度過一個難忘的節日時光。讓愛在這個寒冷的冬季綻放，帶給你溫暖和幸福。'
                 ),
+               
             ]
                 
             line_bot_api.reply_message(
                 event.reply_token,
                 reply_messages
             )
-        elif event_text == "我想了解如何成為 AWS Educate 校園大使":
+        elif event_text == "使用者體驗":
             reply_messages = [
                 TextSendMessage(
-                    text=f'成為AWS Educate校園大使是一個很讚的機會👍 '
+                    text=f'今天配對到的人還滿意嗎？ '
                 ),
                 TextSendMessage(
-                    text=f'你可以積極參與AWS Educate舉辦的大小活動，確保你充分了解AWS雲端運算和相關技術。\n也可以先透過以下連結註冊體驗 AWS Educate\n👉🏻 https://awseducate.tw/2'
+                    text=f'一到五顆星你願意給他幾顆呢？'
                 ),
                 TextSendMessage(
-                    text=f'如果你有興趣成為AWS Educate校園大使，建議你可以定期關注AWS Educate的社群與活動內容，以便瞭解校園大使計劃的最新消息。'
+                    text=f'有留下後續聯絡方式了嗎？'
                 ),
                 TextSendMessage(
-                    text=f'期待你能加入 AWS Educate 校園大使，讓我們一起過程中都能有所收穫 🫵🏻'
+                    text=f'有後續約出來嗎'
                 ),
                 ImageSendMessage(
                     original_content_url = "https://line-workshop-test.s3.amazonaws.com/become_ambassader.jpg",
